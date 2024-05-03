2a8235c122c1689ebc2da1c3bdd982cd8669dc59
Silviajchoi <157684979+Silviajchoi@users.noreply.github.com>
2024-05-03 23:21:51 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -109,7 +109,7 @@ def getDataset2():
         vaccineDistributed = vaccineData.loc[i]['actuals.vaccinesDistributed']
         vaccineInitiated = vaccineData.loc[i]['actuals.vaccinationsInitiated']
         vaccineCompleted = vaccineData.loc[i]['actuals.vaccinationsCompleted']
-        vaccineInitiatedRatio = vaccineData.loc[i]['metrics.vaccinationsInitiatedRatio']
+        hospitalizedCurrently = vaccineData.loc[i]['metrics.vaccinationsInitiatedRatio']
         vaccineCompleteRatio = vaccineData.loc[i]['metrics.vaccinationsCompletedRatio']
     
         if (math.isnan(vaccineData.loc[i]['actuals.vaccinesDistributed'])) :
@@ -173,18 +173,15 @@ def getDataset2():
     st.altair_chart(vaccineCompleteRatio_chart, use_container_width=True)    
 
 def getDataset3():
+    state_abbr = {'CA' : 'California', 'TX' : 'Texas', 'FL' : 'Florida', 'NY' : 'New York', 'IL' : 'Illinois', 'PA' : 'Pennsylvania', 'OH' : 'Ohio', 'NC' : 'North Carolina', 'GA' : 'Georgia', 'MI' : 'Michigan', 'NJ' : 'New Jersey', 'TN' : 'Tennessee', 'AZ' : 'Arizona', 'MA' : 'Massachusetts', 'IN' : 'Indiana', 'VA' : 'Virginia', 'WI' : 'Wisconsin', 'WA' : 'Washington', 'SC' : 'South Carolina', 'MN' : 'Minnesota', 'MO' :  'Missouri', 'CO' : 'Colorado', 'KY' : 'Kentucky', 'AL' : 'Alabama', 'LA' : 'Louisiana', 'OK' : 'Oklahoma', 'MD' : 'Maryland', 'UT' : 'Utah', 'IA' : 'Iowa', 'AR' : 'Arkansas', 'MS' : 'Mississippi', 'KS' : 'Kansas', 'CT' : 'Connecticut', 'NV' : 'Nevada', 'OR' : 'Oregon', 'NM' : 'New Mexico', 'WV' : 'West Virginia', 'NE' : 'Nebraska', 'ID' : 'Idaho', 'RI' : 'Rhode Island', 'NH' : 'New Hampshire', 'MT' : 'Montana', 'DE' : 'Delaware', 'HI' : 'Hawaii', 'AK' : 'Alaska', 'ME' : 'Maine', 'ND' : 'North Dakota', 'SD' : 'South Dakota', 'WY' : 'Wyoming', 'DC' : 'District Of Columbia', 'VT' : 'Vermont'}
     # Public API Read
     url = "https://api.covidtracking.com/v1/states/current.csv"
     csvData = pd.read_csv(url, encoding='cp949')
     stateData = csvData[['state', 'positive', 'probableCases', 'negative', 'hospitalizedCurrently']]
-    # file open
-    f_w = open('state_metadata_cleaned.csv', 'w', encoding="UTF-8", newline='')
-    wr = csv.writer(f_w)
+
+    state_hospitalizedCurrently_info = {}
     
     # data cleaning
-    print('state', 'positive', 'probableCases', 'negative', 'hospitalizedCurrently')
-    wr.writerow(['state', 'positive', 'probableCases', 'negative', 'hospitalizedCurrently'])
-    
     for i in range(stateData.shape[0]):
         state = stateData.loc[i]['state']
         positive = stateData.loc[i]['positive']
@@ -203,71 +200,31 @@ def getDataset3():
         
         if (math.isnan(hospitalizedCurrently)) :
             hospitalizedCurrently = 0
-            
         
         # export cleaned dataset
-        print(state, positive, probableCases, negative, hospitalizedCurrently)
-        wr.writerow([state, positive, probableCases, negative, hospitalizedCurrently])
+        if state in state_abbr:
+            state_hospitalizedCurrently_info[state_abbr[state]] = hospitalizedCurrently
+
+        sorted_hospitalizedCurrently = sorted(state_hospitalizedCurrently_info.items(), key = lambda item: item[1], reverse = True)
+
+        hospitalizedCurrently_state = []
+        hospitalizedCurrently_value = []
+        for hospitalizedCurrently in sorted_vaccineInitiatedRatio:
+            hospitalizedCurrently_state.append(hospitalizedCurrently[0])
+            hospitalizedCurrently_value.append(hospitalizedCurrently[1])
+        
+        hospitalizedCurrently_tb = pd.DataFrame(
+           {"state": hospitalizedCurrently_state, "currently hospitalized": hospitalizedCurrently_value}
+        )
+        hospitalizedCurrently_tb.columns = ["state", 'currently hospitalized']
+
+        hospitalizedCurrently_chart = alt.Chart(hospitalizedCurrently_tb).mark_bar().encode(
+            x=alt.X('state', sort=None),
+            y='currently hospitalized'
+        )
     
-    print("Cleaned Dataset \"state_metadata_cleaned.csv\" is exported")
-
-def mergeDataset():
-    state_abbr = {'CA' : 'California', 'TX' : 'Texas', 'FL' : 'Florida', 'NY' : 'New York', 'IL' : 'Illinois', 'PA' : 'Pennsylvania', 'OH' : 'Ohio', 'NC' : 'North Carolina', 'GA' : 'Georgia', 'MI' : 'Michigan', 'NJ' : 'New Jersey', 'TN' : 'Tennessee', 'AZ' : 'Arizona', 'MA' : 'Massachusetts', 'IN' : 'Indiana', 'VA' : 'Virginia', 'WI' : 'Wisconsin', 'WA' : 'Washington', 'SC' : 'South Carolina', 'MN' : 'Minnesota', 'MO' :  'Missouri', 'CO' : 'Colorado', 'KY' : 'Kentucky', 'AL' : 'Alabama', 'LA' : 'Louisiana', 'OK' : 'Oklahoma', 'MD' : 'Maryland', 'UT' : 'Utah', 'IA' : 'Iowa', 'AR' : 'Arkansas', 'MS' : 'Mississippi', 'KS' : 'Kansas', 'CT' : 'Connecticut', 'NV' : 'Nevada', 'OR' : 'Oregon', 'NM' : 'New Mexico', 'WV' : 'West Virginia', 'NE' : 'Nebraska', 'ID' : 'Idaho', 'RI' : 'Rhode Island', 'NH' : 'New Hampshire', 'MT' : 'Montana', 'DE' : 'Delaware', 'HI' : 'Hawaii', 'AK' : 'Alaska', 'ME' : 'Maine', 'ND' : 'North Dakota', 'SD' : 'South Dakota', 'WY' : 'Wyoming', 'DC' : 'District Of Columbia', 'VT' : 'Vermont'}
-
-    f_r1 = open('web_scrape_states_covid_info_cleaned.csv', 'r', encoding="UTF-8")
-    f_r2 = open('vaccinated_cleaned.csv', 'r', encoding="UTF-8")
-    f_r3 = open('state_metadata_cleaned.csv', 'r', encoding="UTF-8")
-    
-    rdr1 = csv.reader(f_r1)
-    rdr2 = csv.reader(f_r2)
-    rdr3 = csv.reader(f_r3)
-
-    state_info = {}
-    for line in rdr1:
-        if(line[0] != 'state'):
-            total_case = int(line[1].replace(',',''))
-            new_case = 0
-            if (line[2] != '') :
-                new_case = int(line[2].replace(',',''))
-            total_death = int(line[3].replace(',',''))
-            new_death = 0
-            if (line[2] != '') :
-                new_death = int(line[4].replace(',',''))
-
-            death_rate = total_death / total_case * 100
-            state_info[line[0]] = [total_case, new_case, total_death, new_death, death_rate]
-
-
-    for line in rdr2:
-        if line[0] in state_abbr:
-            state_info[state_abbr[line[0]]+ " "].append(float(line[1]))
-            state_info[state_abbr[line[0]]+ " "].append(float(line[2]))
-            state_info[state_abbr[line[0]]+ " "].append(float(line[3]))
-            state_info[state_abbr[line[0]]+ " "].append(float(line[4]) * 100)
-            state_info[state_abbr[line[0]]+ " "].append(float(line[5]) * 100)
-
-    for line in rdr3:
-        if(line[0] != 'state' and line[0] != 'AS' and line[0] != 'GU' and line[0] != 'MP' and line[0] != 'PR' and line[0] != 'VI'):
-            state_info[state_abbr[line[0]]+ " "].append(float(line[1]))
-            state_info[state_abbr[line[0]]+ " "].append(float(line[2]))
-            state_info[state_abbr[line[0]]+ " "].append(float(line[3]))
-            state_info[state_abbr[line[0]]+ " "].append(float(line[4]))
-
-    sorted_state = sorted(state_info.items(), key = lambda item: item[1], reverse = True)
-    temp = []
-    for state in sorted_state:
-        if(state[0] != "USA Total") :
-            temp.append([state[0], state[1][0], state[1][1], state[1][2], state[1][3], state[1][4], state[1][5], state[1][6], state[1][7], state[1][8], state[1][9], state[1][10], state[1][11], state[1][12], state[1][13]])
-    
-    tb = pd.DataFrame(temp)
-    tb.columns = ["state", 'total_case', 'new_case', 'total_death', 'new_death', 'death_rate', 'vaccineDistributed','vaccineInitiated','vaccineCompleted','vaccineInitiatedRatio','vaccineCompleteRatio', 'positive','probableCases','negative','hospitalizedCurrently']
-    # print("Sorted in Descending Order (Total Case)")
-    # print(tb)
-    tb.to_csv('Choi_Silvia_proj2.csv', sep=',')
-         
-    f_r1.close()
-    f_r2.close()
-    f_r3.close()
+        st.write("""currently hospitalized (States)""")
+        st.altair_chart(hospitalizedCurrently_chart, use_container_width=True)
 
 def show_intro():
     st.write("""
@@ -283,6 +240,12 @@ def show_explorer():
     Data2: API
     """)
     getDataset2()
+
+    st.write("""
+    Data3: Public API
+    """)
+    getDataset3()
+    
 def show_conclusions():
     st.write("""
     conclusion
